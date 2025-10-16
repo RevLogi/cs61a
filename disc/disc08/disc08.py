a065c81d14a6b27b7bfad01049e0c02bff6b0986
RevLogi <au5mau6@gmail.com>
2025-10-16 09:25:22 +0800

    update disc08

--- disc/disc08/disc08.py
+++ disc/disc08/disc08.py
@@ -65,6 +65,54 @@ def overlap(s, t):
     else:
         return overlap(s, t.rest)
 
+def display(s, k=10):
+    """Print the first k digits of infinite linked list s as a decimal.
+
+    >>> s = Link(0, Link(8, Link(3)))
+    >>> s.rest.rest.rest = s.rest.rest
+    >>> display(s)
+    0.8333333333...
+    """
+    assert s.first == 0, f'{s.first} is not 0'
+    digits = f'{s.first}.'
+    s = s.rest
+    for _ in range(k):
+        assert s.first >= 0 and s.first < 10, f'{s.first} is not a digit'
+        digits += str(s.first)
+        s = s.rest
+    print(digits + '...')
+
+def divide(n, d):
+    """Return a linked list with a cycle containing the digits of n/d.
+
+    >>> display(divide(5, 6))
+    0.8333333333...
+    >>> display(divide(2, 7))
+    0.2857142857...
+    >>> display(divide(1, 2500))
+    0.0004000000...
+    >>> display(divide(3, 11))
+    0.2727272727...
+    >>> display(divide(3, 99))
+    0.0303030303...
+    >>> display(divide(2, 31), 50)
+    0.06451612903225806451612903225806451612903225806451...
+    """
+    assert n > 0 and n < d
+    result = Link(0)  # The zero before the decimal point
+    cache = {}
+    tail = result
+
+    while n not in cache:
+        q, r = 10 * n // d, 10 * n % d
+        tail.rest = Link(q)
+        tail = tail.rest
+        cache[n] = tail
+        n = r
+
+    tail.rest = cache[n]
+    return result
+
 class Link:
     """A linked list is either a Link object or Link.empty
 
